520529882c1dbe08c3bd4aa3a4d58280a1415d1f
stanisauceg <mishahutson@gmail.com>
2019-02-18 14:41:11 -0500

    projectized the script

--- src/scrapings_cover-to-weight.R
+++ src/scrapings_cover-to-weight.R
@@ -1,26 +1,27 @@
-getwd()
-setwd("C:/Users/Misha/Dropbox/research/2016-17 intertidal/")
-
-search() # check for attached objects, detach() anything unnecessary
-rm(list=ls())
-
-library(data.table)
-data<-fread("cover-wt.csv",header=TRUE)
-data[,1]<-NULL
-colnames(data)[8]<-"fraction"
-
-data$target<-as.factor(data$target)
 library(car)
+library(tidyverse)
 
-lm1 <- lm(iA ~ target + final + iB + iM, data = data)
+path.read.csv <- function(...){
+  data_path <- file.path(...)
+  df <- read_csv(data_path)
+  return(df)
+}
+
+scrapings <- path.read.csv("data", "cover-wt.csv")
+
+colnames(scrapings)[9]<-"fraction"
+
+scrapings$target <- as.factor(scrapings$target)
+
+lm1 <- lm(iA ~ target + final + iB + iM, data = scrapings)
 summary(lm1)
 
-anova.lm1<-anova(lm1)
-aov.lm1<-aov(lm1)
+anova.lm1 <- anova(lm1)
 anova.lm1
-aov.lm1
-summary(aov.lm1)
-summary(anova.lm1)
+
+# aov.lm1 <- aov(lm1)
+# aov.lm1
+# summary(aov.lm1)
 
 avPlots(lm1)
 plot(lm1)
