8c6f58a91062b0cfa602c4e61482a39be1bf002e
stanisauceg <mishahutson@gmail.com>
2019-02-18 17:06:45 -0500

    environmental cleanup using functions

--- src/ordination.R
+++ src/ordination.R
@@ -22,22 +22,22 @@ treat <- id.tags$removal[t0]
 set.seed(24)
 community.mds<-metaMDS(community,distance="bray",trymax=50,k=3)
 
+community.mds
+
 ## check stress plot
 stressplot(community.mds)
 
-
-# create data frame to plot nMDS with ggplot2
-MDS1<-community.mds$points[,1]
-MDS2<-community.mds$points[,2]
-MDS3<-community.mds$points[,3]
-
-NMDS <- tibble(MDS1 = MDS1, MDS2 = MDS2, MDS3 = MDS3, 
-                   Plot = data$plot, Site = data$site, TimeStep = data$TimeStep,
-                   Removal = data$removal, Pred = data$pred)
+## create data frame to plot nMDS with ggplot2
+NMDS <- tibble(MDS1 = community.mds$points[,1], 
+               MDS2 = community.mds$points[,2], 
+               MDS3 = community.mds$points[,3],
+               Plot = data$plot, 
+               Site = data$site, 
+               TimeStep = data$TimeStep,
+               Removal = data$removal,
+               Pred = data$pred)
 summary(NMDS)
 str(NMDS)
-
-rm(MDS1, MDS2, MDS3)
 
 # Rename removal treatments to be more informative. 
 # Currently a factor, so un-factor it first.
@@ -53,7 +53,7 @@ NMDS$Removal[which(NMDS$Removal == "none")] <- "No Removal"
 NMDS$Removal <- factor(NMDS$Removal, levels = c("Fucus", "Barnacle", "Mussel", "No Removal"))
 
 # get the centroids for each grouping
-mds_cols <- cbind(MDS1, MDS2, MDS3)
+mds_cols <- cbind(MDS1 = NMDS$MDS1, MDS2 = NMDS$MDS2, MDS3 = NMDS$MDS3)
 
 centroids <- aggregate(mds_cols ~ Removal * Pred * TimeStep, NMDS, mean) # full factorial, 3 factors
 centroids.r <- aggregate(mds_cols ~ Removal * TimeStep, NMDS, mean) # no predation
@@ -182,8 +182,6 @@ plot_3_timesteps(removal_plot)
 # plot_3_timesteps(pred_by_removal_plot)
 
 
-community.mds
-
 ##### Analysis of Similarities (ANOSIM) ###################
 
 my_anosims <- function(timestep) {
@@ -201,165 +199,24 @@ list(t0, t1, t4) %>% walk(my_anosims)
 set.seed(24)
 adonis.full <- adonis(vegdist(data[,c(5:16)], method="bray")
                     ~ TimeStep*pred*removal, strata=data$site, data=data, permutations=999)
+adonis.full
+
+adonis.2 <- adonis(vegdist(data[,c(5:16)], method="bray")
+                      ~ TimeStep*pred*removal -TimeStep:pred:removal , strata=data$site, data=data, permutations=999)
+adonis.2
+
 
 # split by timestep
-t<-t0 # choose from c(t0, t1, t4)
+adonis_time <- function(timestep) {
+  a <- adonis(vegdist(data[timestep, c(6:16)], method="bray")
+              ~ pred*removal, strata=data$site[timestep], data=data[timestep,], permutations=9999)
+  return(a)
+}
 
-adonis.t0<-adonis(vegdist(data[t,c(6:16)],method="bray")
-                  ~pred*removal, strata=data$site[t], data=data[t], permutations=9999)
-adonis.t0m<-adonis(vegdist(data[t,c(6:8,10:16)],method="bray")
-                   ~pred*removal, strata=data$site[t], data=data[t], permutations=9999)
-adonis.t0b<-adonis(vegdist(data[t,c(6,8:16)],method="bray")
-                   ~pred*removal, strata=data$site[t], data=data[t], permutations=9999)
-adonis.t0f<-adonis(vegdist(data[t,c(7:16)],method="bray")
-                   ~pred*removal, strata=data$site[t], data=data[t], permutations=9999)
+adonis.t0 <- adonis_time(t0)
+adonis.t1 <- adonis_time(t1)
+adonis.t4 <- adonis_time(t4)
 
-adonis.full
 adonis.t0
-adonis.t0$aov.tab
-adonis.t0$aov.tab[6]
-adonis.t0$aov.tab[6][[1]]
-adonis.t0$aov.tab[6][[1]][[3]] # interaction p-val
-adonis.t0$aov.tab[5][[1]][[3]] # interaction r2
 adonis.t1
 adonis.t4
-
-adonis.t4a
-adonis.t4b
-adonis.t4m
-adonis.t4bm
-
-adonis.t0s
-adonis.t1s
-adonis.t4s
-
-adonis2.t0<-adonis2(vegdist(data[t,c(6:16)],method="bray")
-                    ~pred*removal, strata=data$site[t], data=data[t], permutations=9999)
-
-adonis2.t1<-adonis2(vegdist(data[t,c(6:16)],method="bray")
-                    ~pred*removal, strata=data$site[t], data=data[t], permutations=9999)
-
-adonis2.t4<-adonis2(vegdist(data[t,c(6:16)],method="bray")
-                    ~pred*removal, strata=data$site[t], data=data[t], permutations=9999)
-
-
-adonis2.t0
-adonis2.t1
-adonis2.t4
-
-dbrda.t0 <- dbrda(vegdist(data[t,c(6:16)],method="bray")
-                  ~pred*removal, strata=data$site[t], data=data[t])
-anova(dbrda.t0, by = "margin")
-
-
-rmv<-c("none","A","B","M")
-prd<-c("open","roof","cage")
-combo<-expand.grid(rmv, prd)
-colnames(combo)[1]<-"rmv"
-colnames(combo)[2]<-"prd"
-
-data.0<-data[t0]
-
-data.0$permanova.rmplot<-numeric(length=length(t0))
-for(i in 1:length(data.0$plot)){
-  adonis.rmplot<-adonis(vegdist(data.0[-i,c(6:16)],method="bray")
-                        ~pred*removal, strata=data.0$site[-i], data=data.0[-i,], permutations=999)
-  data.0$permanova.rmplot[i]<-adonis.rmplot$aov.tab[6][[1]][[3]]
-}
-data.0$permanova.rmplot
-sort(data.0$permanova.rmplot)
-which(data.0$permanova.rmplot==max(data.0$permanova.rmplot))
-which(data.0$permanova.rmplot==max(data.0$permanova.rmplot[-c(31,50,49,98)]))
-which(data.0$permanova.rmplot==max(data.0$permanova.rmplot[-c(18,50)]))
-# in descending order: 50, 18, 20 & 103 (tied)
-# another time: 31 & 98 (tied), 28 & 59 (tied)
-# last time: 46 & 98 (tied), 59 & 63 & 67
-data.0[which(removal=="none" & pred =="cage"),2:9][order(Mytilus, decreasing=TRUE)]
-data.0[which(removal=="none" & pred =="cage"),2:9][order(barnacle, decreasing=FALSE)]
-which(data.0$plot=="L2-14")
-which(data.0$plot=="L3-22")
-which(data.0$plot=="L2-18")
-which(data.0$plot=="L3-2")
-which(data.0$plot=="L3-18")
-
-
-i<-c(76,75,1,51,69,18) # six plots from none/roof
-i<-c(42, 87, 46, 84, 82) # 5 plots from none/cage
-
-adonis(vegdist(data.0[-i,c(6:16)],method="bray")
-       ~pred*removal, strata=data.0$site[-i], data=data.0[-i,], permutations=9999)
-mean(data.0$Fucus[i])
-mean(data.0$Fucus[-i])
-mean(data.0$Mytilus[i])
-mean(data.0$Mytilus[-i])
-mean(data.0$barnacle[i])
-mean(data.0$barnacle[-i])
-sort(data.0$plot[i])
-
-data.4<-data[t4]
-i<-which((data.4$removal=="M" | data.4$removal=="B") & data.4$pred=="open") # six plots from none/roof
-adonis(vegdist(data.4[,c(6:16)],method="bray")
-       ~pred*removal, strata=data.4$site, data=data.4, permutations=9999)
-adonis(vegdist(data.4[-i,c(6:16)],method="bray")
-       ~pred*removal, strata=data.4$site[-i], data=data.4[-i,], permutations=9999)
-
-
-impt<-which(data.0$permanova.rmplot>0.013)
-data.0$removal[impt]
-data.0$pred[impt]
-data.0$plot[impt]
-
-permanova.rmcombo<-numeric(length=length(combo[,1]))
-r2.rmcombo<-numeric(length=length(combo[,1]))
-for (i in 1:length(permanova.rmcombo)) {
-  sp<-combo[i,1]
-  pred<-combo[i,2]
-  data.c<-data.0[-which(data.0$removal==sp & data.0$pred==pred),]
-  adonis.rmcombo<-adonis(vegdist(data.c[,c(6:16)],method="bray")
-                         ~pred*removal, strata=data.c$site, data=data.c, permutations=999)
-  permanova.rmcombo[i] <- adonis.rmcombo$aov.tab[6][[1]][[3]]
-  r2.rmcombo[i] <- adonis.rmcombo$aov.tab[5][[1]][[3]]
-}
-permanova.rmcombo
-r2.rmcombo
-impt<-which(permanova.rmcombo>0.05)
-which(permanova.rmcombo > 0.05)
-r2.rmcombo[which(permanova.rmcombo > 0.05)]
-
-combo[c(1,5,9),]
-which(data.0$permanova.rmplot==max(data.0$permanova.rmplot))
-data.0$plot[50]
-data.0$removal[50]
-data.0$pred[50]
-data.0$plot[which(data.0$removal=="none" & data.0$pred=="cage")]
-
-
-adonis.rmplot<-adonis(vegdist(data.0[-i,c(6:16)],method="bray")
-                      ~pred*removal, strata=data.0$site[-i], data=data.0[-i,], permutations=999)
-
-adonis(vegdist(data.0[,c(6:16)],method="bray")~pred*removal, strata=data.0$site,data=data.0,permutations=999)
-
-data.no<-data.0[-which(data.0$removal=="none" & data.0$pred=="open"),]
-adonis(vegdist(data.no[,c(6:16)],method="bray")
-       ~pred*removal, strata=data.no$site, data=data.no, permutations=999) # still sig
-data.nr<-data.0[-which(data.0$removal=="none" & data.0$pred=="roof"),]
-adonis(vegdist(data.nr[,c(6:16)],method="bray")
-       ~pred*removal, strata=data.nr$site, data=data.nr, permutations=999) # sig/ marginally sig
-data.nc<-data.0[-which(data.0$removal=="none" & data.0$pred=="cage"),]
-adonis(vegdist(data.nc[,c(6:16)],method="bray")
-       ~pred*removal, strata=data.nc$site, data=data.nc, permutations=999) # still sig
-
-adonis(vegdist(data.4[,c(6:16)],method="bray")
-       ~pred*removal, strata=data.4$site, data=data.4, permutations=9999)
-i<-which((data.4$removal=="M" | data.4$removal == "B") & data.4$pred=="open") # 18 plots from (-M|-B) & open
-adonis(vegdist(data.4[-i,c(6:16)],method="bray")
-       ~pred*removal, strata=data.4$site[-i], data=data.4[-i,], permutations=9999) # test w/o the above plots
-adonis(vegdist(data.4[,c(6:8,10:16)],method="bray")
-       ~pred*removal, strata=data.4$site, data=data.4[,-9], permutations=9999) # test w/o mussels in community
-
-
-
-adonis(vegdist(data.mo[,c(6:16)],method="bray")
-       ~pred*removal, strata=data.mo$site, data=data.mo, permutations=999) # still sig
-
-
