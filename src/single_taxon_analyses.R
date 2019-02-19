bdba47cad90253e724eab712a20fb9ec228c3925
stanisauceg <mishahutson@gmail.com>
2019-02-19 10:23:44 -0500

    translate code from `data.table` to `tidyr` notation

--- src/single_taxon_analyses.R
+++ src/single_taxon_analyses.R
@@ -1,30 +1,46 @@
 ### create data frame showing cover of each target species over time
-# melt:
-mdat <- data.table::melt(data, id.vars= c("site","plot","TimeStep","date","pred","removal"),
-             measure.vars = c("Fucus","Mytilus","barnacle"), 
-             variable.name="species", value.name="pct.cover")
+mdat <- data %>% 
+  gather(key = "species", value = "pct_cover", Fucus, barnacle, Mytilus) %>%
+  select(TimeStep:plot, removal:pct_cover) %>%
+  mutate(sp_t = factor(paste(species, TimeStep, sep = "-")))
+
 head(mdat)
 summary(mdat)
 str(mdat)
 
-mdat$sp_t <- paste(mdat$species, mdat$TimeStep, sep="-")
-mdat$sp_t <- factor(mdat$sp_t)
-
+# dictionary for species names
 species_names <- c("barnacle" = "Barnacle", 
                   "Fucus" = "Fucus", 
-                  "Mytilus" = "Mytilus", 
+                  "Mytilus" = "Mussel", 
                   "Control" = "Control")
 
 dodge <- position_dodge(width = 0.1)
 
-ggplot(mdat, aes(x = TimeStep, y = pct.cover, color = removal, pch = pred)) +
+ggplot(mdat, aes(x = TimeStep, y = pct.cover, color = removal)) +
   stat_summary(fun.y = mean, geom = "point", size = 2, position = dodge) +
-  stat_summary(fun.y = mean, geom = "line", aes(group = interaction(removal, pred)), 
+  stat_summary(fun.y = mean, geom = "line", aes(group = removal), 
                linetype = 2, position = dodge) +
   stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.1, position = dodge) +
-  facet_wrap(~species, nrow = 3, labeller = as_labeller(species_names)) +
+  facet_wrap(~ species, nrow = 3, labeller = as_labeller(species_names)) +
   scale_x_discrete(name = "Time Step", labels = c("Initial", "Treatment", "Recovery")) +
   ylab("Percent Cover") +
-  scale_color_manual(name="Removal", labels = c("Fucus", "Barnacle", "Mytilus", "Control"),
+  scale_color_manual(name = "Removal", labels = c("Fucus", "Barnacle", "Mussel", "Control"),
                      values = c("#7CAE00", "#F8766D", "#00BFC4", "#C77CFF")) +
   theme_bw(base_size = 18)
+
+
+# mdat2 <- data %>% 
+#   gather(key = "species", value = "pct_cover", bare) %>%
+#   select(TimeStep:plot, removal:pct_cover) 
+# 
+# ggplot(mdat2, aes(x = TimeStep, y = pct_cover, color = removal)) +
+#   stat_summary(fun.y = mean, geom = "point", size = 2, position = dodge) +
+#   stat_summary(fun.y = mean, geom = "line", aes(group = removal), linetype = 2, position = dodge) +
+#   stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.1, position = dodge) +
+#   xlab("Time Step") +
+#   ylab("Percent Bare Space") +
+#   scale_color_manual(name="Removal", labels = c("Fucus", "Barnacle", "Mytilus", "Control"),
+#                      values = c("#7CAE00", "#F8766D", "#00BFC4", "#C77CFF")) +
+#   scale_x_discrete(labels = c("Initial", "Treatment", "Recovery")) +
+#   theme_bw(base_size = 18) +
+#   theme(axis.text.x = element_text(hjust = 0.5, vjust = 1))
